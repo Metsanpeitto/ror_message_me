cba1a14c865fcbee561b8aef3c45d26280a4352f
Metsanpeitto <abraham.asturies@gmail.com>
2021-09-18 22:07:06 +0200

    Add broadcasting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,21 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    };
+  });
+};
+
 
 $(document).on('turbolinks:load', function(){
   $('.ui.dropdown').dropdown();
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,10 @@ class MessagesController < ApplicationController
 
   def create
     message = current_user.messages.build(message_params)
-    redirect_to root_path if message.save
+    if message.save
+      ActionCable.server.broadcast 'chatroom_channel',
+                                   mod_message: message_render(message)
+    end
   end
 
   private
