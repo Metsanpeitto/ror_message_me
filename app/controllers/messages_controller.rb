26feaf819776429d332e68620c69e859839f8e75
Metsanpeitto <abraham.asturies@gmail.com>
2021-09-18 23:01:41 +0200

    Fix Action.cable syntax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,8 +4,8 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      ActionCable.server.broadcast 'chatroom_channel',
-                                   mod_message: message_render(message)
+      ActionCable.server.broadcast \
+        'chatroom_channel', { mod_message: message_render(message) }
     end
   end
 
